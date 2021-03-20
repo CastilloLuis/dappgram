4acfe3e1dfeef62c495fa90b12257f1b9059208d
Luis Castillo <iluiscastillo@MacBook-Pro-de-Luis.local>
2021-03-19 22:27:08 -0500

    inital setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface AppProps {}
+
+const App: React.FC<AppProps> = () => {
+  return (
+    <div>
+      Hello World!
+    </div>
+  )
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: ['./node_modules/babel-polyfill', './src/index.tsx'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].bundle.js'
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.ts','.tsx','.js', '.jsx', '.json'],
+  },
+  devServer: {
+    port: 3000,
+    historyApiFallback: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: path.resolve(__dirname, 'node_modules'),
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new webpack.IgnorePlugin(/^(?:electron|ws)$/)
+  ]
+}
